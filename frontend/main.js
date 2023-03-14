cc3595378efe4084a6431c7356612d5232192b9e
GioGio4 <g.ceccobelli@outlook.it>
2023-03-14 19:13:02 +0100

    JS chiamata axios verso api.php recupero la lista

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,19 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      // Array che riceverà la lista di dischi dalla chiamata axios verso api.php (che a sua volta contiene una lista importata dal file disk-list.json)
+      diskList: [],
+    };
+  },
+
+  created() {
+    axios
+      .get("http://localhost/php-dischi-json/API/api.php")
+      .then((response) => {
+        this.diskList = response.data;
+
+        // console.log(this.diskList);
+      });
   },
 }).mount("#app");
